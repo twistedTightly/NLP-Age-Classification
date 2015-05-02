0e4b206ae29db0acf70c2bfd3edd0b1b8dc30d44
Jennifer Long <jlong8@nd.edu>
2015-05-02 16:08:35 -0400

    Adding the POS tagger, classifier, and combined linear regression classifier

---
+++ src/linear_regression_classifier.py
@@ -0,0 +1,76 @@
+#Linear Regression: combines probabilities from naive_bayes_class_return_probs.py and pos_classifier_4grams_return_probs.py, using our POS tagger
+
+from naive_bayes_class_return_probs import naive_bayes
+from pos_tagger_class import pos_tagger
+from pos_classifier_4grams_return_probs import pos_classifier
+
+#Set filenames
+trainFilename = "all_posts_train"
+testFilename = "all_posts_test"
+
+#Read in training file
+trainFile = open(trainFilename, 'r')
+trainData = trainFile.readlines()
+trainFile.close()
+
+#Create naive bayes classifier
+naive_bayes = naive_bayes(trainData)
+
+#Create part of speech tagger
+pos_tagger = pos_tagger(trainData)
+
+#Create tag sequence classifier
+pos_classifier = pos_classifier(trainData)
+
+#Read in testing file
+testFile = open(testFilename, 'r')
+testData = testFile.readlines()
+testFile.close()
+
+numCorrect = 0
+numIncorrect = 0
+
+for line in testData:
+	words = line.rstrip().split()
+
+	#Separate first element (category) from the rest of the words
+	actualCategory = words[0]
+	words = words[1:]
+
+	#Get vocab probs from naive bayes model
+	vocab_probs = naive_bayes.decode(words)
+	
+	#Get POS probs by generating the tag sequence and plugging it into the tri- and 4-grams POS classifier
+	tagSequence = pos_tagger.decode(words)
+	pos_probs = pos_classifier.decode(tagSequence)
+	
+	#Calculate linear regression model
+
+	#Straight model, no ridge regression or other multicollinearity corrections- leaves out 40s variables
+	#predictedAge = 41.4861 - 32.3173*vocab_probs[0] - 22.4564*vocab_probs[1] - 9.166*vocab_probs[2] + 4.2246*pos_probs[0] + 3.2997*pos_probs[1] - 13.55*pos_probs[2]
+	
+	#Using ridge regression, lambda = 1: 43.2% accuracy
+	#predictedAge = 26.8377018 - 17.8059727*vocab_probs[0] - 7.9473321*vocab_probs[1] + 5.3389428*vocab_probs[2] + 14.5049926*vocab_probs[3] + 4.3542027*pos_probs[0] + 3.4327589*pos_probs[1] - 13.3937298*pos_probs[2] + 0.1450386*pos_probs[3]
+
+	print predictedAge
+
+	if predictedAge <= 15:
+		winningCategory = '10'
+	elif predictedAge <= 25:
+		winningCategory = '20'
+	elif predictedAge <= 35:
+		winningCategory = '30'
+	else:
+		winningCategory = '40'
+	
+
+	#winningIndex = vocab_probs.index(max(vocab_probs))
+	#winningCategory = naive_bayes.categories[winningIndex]
+
+	#Evaluate accuracy
+	if winningCategory == actualCategory:
+		numCorrect += 1
+	else:
+		numIncorrect += 1
+
+print str( float(numCorrect)/(numCorrect + numIncorrect) * 100 ) + "%"
---
+++ src/pos_classifier_4grams_return_probs.py
@@ -0,0 +1,230 @@
+#Method 3: POS tagging with naive bayes bigrams classifier
+
+import math
+
+def addLogProbs(logp, logq):
+	if logp > logq:
+		bigger = logp
+		smaller = logq
+	else:
+		bigger = logq
+		smaller = logp
+	return bigger + math.log10(1 + math.pow(10, smaller - bigger))
+
+class pos_classifier:
+	def __init__(self, trainData = None):
+		if trainData != None: #Train if the training data has been provided
+			self.train(trainData)
+
+	def train(self, trainingData):
+		#Set categories
+		categories = ['10', '20', '30', '40']
+
+		#Create data structures for word counts and category counts
+		wordCounts = dict()
+		categoryDocumentCounts = dict()
+		categoryWordCounts = dict()
+		wordProbs = dict()
+		for category in categories:
+			wordCounts[category] = dict()
+			categoryDocumentCounts[category] = 0
+			categoryWordCounts[category] = 0
+			wordProbs[category] = dict()
+		numDocuments = 0
+		vocabSet = set()
+
+		#Read through each line; count and divide
+		for line in trainingData:
+			words = line.rstrip().split()
+	
+			#Separate first element (category) from the rest of the words
+			category = words[0]
+			words = words[1:]
+
+			#Process the category
+			if category not in categories:
+				continue
+			categoryDocumentCounts[category] += 1
+			numDocuments += 1
+
+			#Construct tag sequence
+			tagSequence = ["<s>"]
+			for word in words:
+				parts = word.split("/")
+				pos = parts[1]
+				tagSequence.append(pos)
+			tagSequence.append("</s>")
+
+			#Construct all of the tag n-grams that can be used, for n=3,4
+			tagNGrams = []
+
+			#Add trigrams
+			prevPrevTag = tagSequence[0]
+			prevTag = tagSequence[1]
+			for tag in tagSequence[2:]:
+				tagTrigram = prevPrevTag + "/" + prevTag + "/" + tag
+				tagNGrams.append(tagTrigram)	
+				prevPrevTag = prevTag
+				prevTag = tag	
+
+			#Add 4-grams (only if the tag sequence has at least 4 tags, meaning it's at least two words plus <s> and </s>
+			if len(tagSequence) >= 4:
+				prevPrevPrevTag = tagSequence[0]
+				prevPrevTag = tagSequence[1]	
+				prevTag = tagSequence[2]
+				for tag in tagSequence[3:]:
+					tag4Gram = prevPrevPrevTag + "/" + prevPrevTag + "/" + prevTag + "/" + tag
+					tagNGrams.append(tag4Gram)
+					prevPrevPrevTag = prevPrevTag
+					prevPrevTag = prevTag
+					prevTag = tag	
+
+			for tagNGram in tagNGrams:
+
+				if tagNGram not in wordCounts[category]:
+					wordCounts[category][tagNGram] = 0
+
+				wordCounts[category][tagNGram] += 1
+				categoryWordCounts[category] += 1
+				vocabSet.add(tagNGram)
+
+		#Set up smoothing
+		delta = 100
+		vocabSize = len(vocabSet)
+
+		#Calculate probabilities for each category
+		categoryProbs = dict()
+		for category in categories:
+			categoryProbs[category] = float(categoryDocumentCounts[category]) / numDocuments
+	
+		#Calculate probabilities for each word
+		for category in categories:
+			for word in wordCounts[category]:
+				wordProbs[category][word] = ( float(wordCounts[category][word]) + delta ) / ( categoryWordCounts[category] + vocabSize*delta )
+
+		#Calculate probabilities for unknown words
+		unknownWordProb = dict()
+		for category in categories:
+			unknownWordProb[category] = float(delta) / ( categoryWordCounts[category] + vocabSize*delta )
+
+		#Save relevant data structures
+		self.wordProbs = wordProbs
+		self.unknownWordProb = unknownWordProb
+		self.categories = categories
+
+	def decode(self, tagSequence):
+		#Get relevant data structures
+		wordProbs = self.wordProbs
+		unknownWordProb = self.unknownWordProb
+		categories = self.categories
+
+		#Construct all of the tag n-grams that can be used, for n=3,4
+		tagNGrams = []
+
+		#Add trigrams
+		prevPrevTag = tagSequence[0]
+		prevTag = tagSequence[1]
+		for tag in tagSequence[2:]:
+			tagTrigram = prevPrevTag + "/" + prevTag + "/" + tag
+			tagNGrams.append(tagTrigram)	
+			prevPrevTag = prevTag
+			prevTag = tag	
+
+		#Add 4-grams (only if the tag sequence has at least 4 tags, meaning it's at least two words plus <s> and </s>
+		if len(tagSequence) >= 4:
+			prevPrevPrevTag = tagSequence[0]
+			prevPrevTag = tagSequence[1]	
+			prevTag = tagSequence[2]
+			for tag in tagSequence[3:]:
+				tag4Gram = prevPrevPrevTag + "/" + prevPrevTag + "/" + prevTag + "/" + tag
+				tagNGrams.append(tag4Gram)
+				prevPrevPrevTag = prevPrevTag
+				prevPrevTag = prevTag
+				prevTag = tag	
+
+		#Add up log probabilities for each word, for each category:
+		thisLineLogProb = dict()
+		for category in categories:
+			thisLineLogProb[category] = 0	
+
+
+			for tagNGram in tagNGrams:
+				#Increment probability
+				if tagNGram in wordProbs[category]:
+					thisWordProb = wordProbs[category][tagNGram]
+				else:
+					thisWordProb = unknownWordProb[category]
+				thisLineLogProb[category] += math.log10(thisWordProb)
+
+		#Get log sum of the 4 log probabilities
+		runningLogSum = thisLineLogProb[categories[0]]
+		for category in categories[1:]:
+			runningLogSum = addLogProbs(runningLogSum, thisLineLogProb[category])
+		totalLogSum = runningLogSum
+		
+		#Construct 4-tuple to be returned of the probability of each category
+		probsToReturn = []
+		for category in categories:
+			thisLogProb = thisLineLogProb[category] - totalLogSum
+			thisProb = math.pow(10, thisLogProb)
+			probsToReturn.append(thisProb)
+
+		return probsToReturn
+
+if __name__ == "__main__":
+	
+	#Set filenames
+	trainFilename = "all_posts_train"
+	testFilename = "all_posts_test"
+
+	#Read in training file
+	trainFile = open(trainFilename, 'r')
+	trainData = trainFile.readlines()
+	trainFile.close()
+
+	#Create pos_classifier
+	pos_classifier = pos_classifier(trainData)
+
+	#Read in testing file
+	testFile = open(testFilename, 'r')
+	testData = testFile.readlines()
+	testFile.close()
+
+	numCorrect = 0
+	numIncorrect = 0
+
+	for line in testData:
+		words = line.rstrip().split()
+	
+		#Separate first element (category) from the rest of the words
+		actualCategory = words[0]
+		words = words[1:]
+
+		#Construct tag sequence
+		tagSequence = ["<s>"]
+		for word in words:
+			parts = word.split("/")
+			pos = parts[1]
+			tagSequence.append(pos)
+		tagSequence.append("</s>")
+
+		probs = pos_classifier.decode(tagSequence)
+
+		winningIndex = probs.index(max(probs))
+		winningCategory = pos_classifier.categories[winningIndex]
+
+		#Evaluate accuracy
+		if winningCategory == actualCategory:
+			numCorrect += 1
+		else:
+			numIncorrect += 1
+
+	print str( float(numCorrect)/(numCorrect + numIncorrect) * 100 ) + "%"
+
+
+
+
+
+
+
+
